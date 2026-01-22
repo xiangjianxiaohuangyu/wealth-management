04a12282ec26200f4c181490d0a2bd2d25319eda
xiangjianxiaohuangyu <ziporynL@gmail.com>
2026-01-22 17:48:30 +0800

    test updater

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + v0.0.1</h1>
+      <h1>Vite + React + v0.0.3</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
